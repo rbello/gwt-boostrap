7e04b2a48702094876f3f668f156952abed0b4c8
R. BELLO <remi@evolya.fr>
2019-04-06 19:59:24 +0200

    Regroupement en 1 sequence

--- build.sh
+++ build.sh
@@ -1,30 +1,57 @@
 #/bin/bash
 
-# Usage: <postgres version> <tomcat version>
+docker pull postgres:9
+docker pull tomcat:8
 
-DIR_BASE="/opt/cim3/"
-PROG_NAME=$0
+docker network create cim3-network
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-CYAN='\033[0;36m'
-YELLOW='\033[0;33m'
-B='\033[1m'
-NC='\033[0m'
+#docker swarm init
 
-OS_NAME=$(uname -s)
-OS_VERSION=$(uname -r)
-printf "Plateforme   : [${GREEN}OK${NC}] OS=${OS_NAME} Version=${OS_VERSION}\n"
+DB1_USER=cim3
+DB1_PASSWORD=XXX
 
-printf -- "--- [${YELLOW} Construction du réseau de communication interne à l'application... ${NC}] ---\n"
-docker network create gwt-network
+DB2_USER=cim3
+DB2_PASSWORD=XXX
 
-printf -- "--- [${YELLOW} Construction de la base de données Postgres v${1}... ${NC}] ---\n"
-docker kill gwt-database
-docker rm gwt-database
-docker pull postgres:$1
+echo "Nettoyage..."
+docker kill cim3-data-1
+docker rm cim3-data-1
 
-printf -- "--- [${YELLOW} Construction du serveur d'application Tomcat v${2}... ${NC}] ---\n"
-docker kill gwt-appsrv
-docker rm gwt-appsrv
-docker pull tomcat:$2
+docker kill cim3-data-2
+docker rm cim3-data-2
+
+docker kill cim3-gestion
+docker rm cim3-gestion
+
+echo "Lancement des bases de données 1 et 2"
+docker run \
+	--name "cim3-data-1" \
+	--net "cim3-network" \
+	--volume /opt/cim3/database/1:/var/lib/postgresql/data \
+	--detach postgres:9
+
+docker run \
+	--name "cim3-data-1" \
+	--net "cim3-network" \
+	--volume /opt/cim3/database/2:/var/lib/postgresql/data \
+	--detach postgres:9
+
+echo "Installation des données sur la base 1"
+docker cp my_data1.backup cim3-data-1:/backups
+docker exec -it cim3-data-1 psql -U postgres -c "CREATE DATABASE cim3db1;"
+docker exec -it cim3-data-1 -U postgres -c \
+ "CREATE USER ${DB1_USER} SUPERUSER PASSWORD '${DB1_PASSWORD}'; \
+  GRANT ALL PRIVILEGES ON DATABASE cim3db1 TO ${DB1_USER};"
+docker exec cim3-data-1 psql -U postgres -l
+docker exec cim3-data-1 pg_restore -U postgres -d cim3db1 /backups/my_data1.backup
+
+echo "Installation des données sur la base 2"
+docker cp my_data2.backup cim3-data-2:/backups
+docker exec -it cim3-data-2 psql -U postgres -c "CREATE DATABASE cim3db2;"
+docker exec -it cim3-data-2 -U postgres -c \
+ "CREATE USER ${DB2_USER} SUPERUSER PASSWORD '${DB2_PASSWORD}'; \
+  GRANT ALL PRIVILEGES ON DATABASE cim3db1 TO ${DB2_USER};"
+docker exec cim3-data-2 psql -U postgres -l
+docker exec cim3-data-2 pg_restore -U postgres -d cim3db2 /backups/my_data2.backup
+
+echo 
