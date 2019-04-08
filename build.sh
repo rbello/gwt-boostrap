84fdc67bdcbcd1d6e1605e3a7a3828e30e069a02
rbello <remi.bello@trustingenierie.com>
2019-04-08 19:02:16 +0200

    Connexion BD ok

--- build.sh
+++ build.sh
@@ -15,6 +15,7 @@ CYAN='\033[0;36m'
 YELLOW='\033[0;33m'
 B='\033[1m'
 NC='\033[0m'
+UL='\033[4m'
 
 printf "${YELLOW}Recupération des images docker...${NC}\n"
 
@@ -42,7 +43,7 @@ docker rm cim3-data-2
 docker kill cim3-gestion
 docker rm cim3-gestion
 
-printf "${YELLOW}Lancement des bases de données 1 et 2${NC}\n"
+printf "${YELLOW}Lancement des bases de données...${NC}\n"
 docker run \
 	--name "cim3-data-1" \
 	--net "cim3-network" \
@@ -59,7 +60,7 @@ docker run \
 
 sleep 10
 
-printf "${YELLOW}Installation des données sur la base 1 : cim_admin${NC}\n"
+printf "${YELLOW}Installation des données sur la base : cim_admin${NC}\n"
 DB1_DATAFILE=cim_admin.backup
 docker exec cim3-data-1 mkdir /backups
 docker cp "database/$DB1_DATAFILE" cim3-data-1:/backups
@@ -72,15 +73,15 @@ docker exec cim3-data-1 pg_restore -U postgres -d cim_admin "/backups/$DB1_DATAF
 docker exec cim3-data-1 psql -U postgres
 
 #printf "${YELLOW}Installation des données sur la base 2${NC}\n"
-#DB2_DATAFILE=cim_01004_amberieu_en_bugey.backup
+DB2_DATAFILE=cim_01004_amberieu_en_bugey.backup
 #docker exec cim3-data-2 mkdir /backups
-#docker cp "database/$DB2_DATAFILE" cim3-data-2:/backups
+#docker cp "database/$DB2_DATAFILE" cim3-data-1:/backups
 #docker exec -it cim3-data-2 psql -U postgres -c "CREATE DATABASE cim3db2;"
 #docker exec -it cim3-data-2 psql -U postgres -c \
 # "CREATE USER ${DB2_USER} SUPERUSER PASSWORD '${DB2_PASSWORD}'; \
 #  GRANT ALL PRIVILEGES ON DATABASE cim3db2 TO ${DB2_USER};"
 #docker exec cim3-data-2 psql -U postgres -l
-#docker exec cim3-data-2 pg_restore -U postgres -d cim3db2 "/backups/$DB2_DATAFILE"
+#docker exec cim3-data-1 pg_restore -U postgres -d cim_admin "/backups/$DB2_DATAFILE"
 
 printf "${YELLOW}Configuration des outils d'administration...${NC}\n"
 ADMIN_PWD=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
@@ -90,8 +91,6 @@ printf "  ${RED}Manager TOMCAT${NC} : admin / $ADMIN_PWD\n"
 
 printf "${YELLOW}Installation de l'application Web${NC}\n"
 cd /opt/cim3/back
-printf "  ${RED}Credentials DB 1 :${NC} $DB1_USER / $DB1_PASSWORD\n"
-printf "  ${RED}Credentials DB 2 :${NC} $DB2_USER / $DB2_PASSWORD\n"
 docker build -t cim3-back/v3 .
 JAVA_OPTS=""
 docker run \
@@ -103,11 +102,17 @@ docker run \
 	-p 80:8080/tcp \
 	-d "cim3-back/v3"
 
-printf "   ${RED}Application CIM3 :${NC} http://$HOSTNAME.ovh.net:80/\n"
-printf "   ${RED}Manager TOMCAT   :${NC} http://$HOSTNAME.ovh.net/manager/html\n"
-#printf "   ${RED}Manager POSTGRES :${NC} http://$HOSTNAME.ovh.net:8080/\n"
 printf "${YELLOW}L'application est lancée !${NC}\n"
+printf "${YELLOW}  Connectez vous sur :${NC} ${UL}http://${HOSTNAME}.ovh.net/gestion/${NC}\n"
+printf "  ${RED}Credentials DB :${NC} host: cim3-data-1  login: ${DB1_USER}  password: ${DB1_PASSWORD}\n"
+printf "  ${RED}Manager POSTGRES directlry: docker exec -it cim3-data-1 psql -U cim3 cim_admin${NC}\n"
+printf "  ${RED}Manager TOMCAT :${NC} http://${HOSTNAME}.ovh.net/manager/html (login: admin  password: ${ADMIN_PWD})\n"
 
 #TODAY=$(date +"%Y-%m-%d")
 #tail -f "/opt/cim3/logs/catalina.$TODAY.log"
 
+# docker exec -it cim3-data-1 psql -U cim3 cim_admin -c "select * from utilisateur"
+# docker exec -it cim3-data-1 psql -U cim3 cim_admin -c "select * from utilisateur where nom = 'trust'"
+# Pwd : fbf81a82a77da87d2b2fbce713d56dc6  (2812)
+# docker exec -it cim3-data-1 psql -U cim3 cim_admin -c "insert into commune_accessible values (734, 2812)"
+# docker exec -it cim3-data-1 psql -U cim3 cim_admin -c "select * from commune_accessible where id_utilisateur = 2812"
